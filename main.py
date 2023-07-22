e01311b61d99e1b081ed08efd027ab4934120b4e
devanshukla <devanshukla0@gmail.com>
2023-07-22 14:56:04 +0000

    Added the whole data
    Maked the job opening website

--- main.py
+++ main.py
@@ -1,11 +1,38 @@
-from flask import Flask
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
+jobs = [{
+  'id': 1,
+  'title': "Software Developer",
+  'Location': "Bengaluru,India",
+  'Salary': 1000000
+}, {
+  'id': 2,
+  'title': "Data Analyst",
+  'Location': "Bengaluru,India",
+  'Salary': 1000000
+}, {
+  'id': 3,
+  'title': "Marketing",
+  'Location': "Bengaluru,India",
+  'Salary': 1000000
+}, {
+  'id': 4,
+  'title': "Food Analayst",
+  'Location': "Bengaluru,India",
+  'Salary': 1000000
+}]
 
-@app.route('/')
+
+@app.route("/")
 def func():
-  return "hello jovian"
+  return render_template('home.html', jobs=jobs)
+
+
+@app.route("/api/jobs")
+def apipoint():
+  return jsonify(jobs)
 
 
 if __name__ == '__main__':
